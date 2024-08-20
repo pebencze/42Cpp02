0d6e493c44147b2da1ba0ef4c1f6e8b5ffd8056c
pebencze <petra.bncz@gmail.com>
2024-08-20 16:00:06 +0200

    ex02: added main

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,28 +6,55 @@
 /*   By: pbencze <pbencze@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 16:03:46 by pbencze           #+#    #+#             */
-/*   Updated: 2024/08/20 15:56:53 by pbencze          ###   ########.fr       */
+/*   Updated: 2024/08/20 15:59:50 by pbencze          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Fixed.hpp"
 
-int main( void ) {
+#include <iostream>
+#include "Fixed.hpp"
+
+int main() {
     Fixed a;
-    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-	Fixed c(3);
-	Fixed d(0);
+    Fixed b(10);
+    Fixed c(42.42f);
+    Fixed d(b);
 
-    std::cout << a << std::endl;
-    //std::cout << a.getRawBits() << std::endl;
-    std::cout << ++a << std::endl;
-    std::cout << a << std::endl;
-    std::cout << a++ << std::endl;
-    std::cout << a << std::endl;
-    std::cout << b << std::endl;
+    // Test assignment operator
+    a = Fixed(1234.4321f);
 
-    std::cout << Fixed::max( a, b ) << std::endl;
+    // Test comparison operators
+    std::cout << "b < c: " << (b < c) << std::endl;
+    std::cout << "b > c: " << (b > c) << std::endl;
+    std::cout << "b >= d: " << (b >= d) << std::endl;
+    std::cout << "b <= d: " << (b <= d) << std::endl;
+    std::cout << "b == d: " << (b == d) << std::endl;
+    std::cout << "b != c: " << (b != c) << std::endl;
+
+    // Test arithmetic operators
+    Fixed e = b + c;
+    Fixed f = c - b;
+    Fixed g = b * c;
+    Fixed h = c / b;
+
+    std::cout << "b + c: " << e << std::endl;
+    std::cout << "c - b: " << f << std::endl;
+    std::cout << "b * c: " << g << std::endl;
+    std::cout << "c / b: " << h << std::endl;
+
+    // Test increment and decrement operators
+    Fixed i;
+    std::cout << "i: " << i << std::endl;
+    std::cout << "i++: " << i++ << std::endl;
+    std::cout << "++i: " << ++i << std::endl;
+    std::cout << "i--: " << i-- << std::endl;
+    std::cout << "--i: " << --i << std::endl;
+
+    // Test min and max functions
+    std::cout << "min(b, c): " << Fixed::min(b, c) << std::endl;
+    std::cout << "max(b, c): " << Fixed::max(b, c) << std::endl;
 
     return 0;
 }
